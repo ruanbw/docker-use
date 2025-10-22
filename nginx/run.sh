20d7991227f526786a78a55810bb4cd574bac67f
ruanbw <1813967922@qq.com>
2025-10-22 12:50:32 +0800

    fix: nginx config

--- nginx/run.sh
+++ nginx/run.sh
@@ -3,4 +3,5 @@ docker run -d \
   -p 80:80 \
   -v $(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf \
   -v $(pwd)/nginx/conf.d:/etc/nginx/conf.d \
+  -v $(pwd)/nginx/frontend:/var/www/frontend \
   nginx
